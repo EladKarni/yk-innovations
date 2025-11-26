284b94c0bd6a5f79c5e92f68f9cae409bf65d39c
Elad Karni <elad.karni@gmail.com>
2025-11-25 20:25:22 -0500

    feat: Add layout and project pages with fallback data for improved user experience

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -4,8 +4,7 @@ import { Inter } from "next/font/google";
 import Footer from "@/components/footer";
 import Navbar from "@/components/navbar";
 import NavLinks from "@/components/NavLinks";
-import "./globals.css";
-import Head from "next/head";
+import "../globals.css";
 import { cn } from "@/util/utils";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -13,31 +12,28 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "YK Innovations",
   description: "YK Innovations is here to help you bring your ideas to life",
+  icons: {
+    icon: [
+      { url: "/favicon-96x96.png", sizes: "96x96", type: "image/png" },
+      { url: "/favicon.svg", type: "image/svg+xml" },
+    ],
+    apple: [
+      { url: "/apple-touch-icon.png", sizes: "180x180" },
+    ],
+  },
+  manifest: "/site.webmanifest",
+  appleWebApp: {
+    title: "YK Innovations",
+  },
 };
 
-export default function RootLayout({
+export default function PublicLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
     <html lang="en" data-theme="mytheme" className="scroll-smooth">
-      <Head>
-        <link
-          rel="icon"
-          type="image/png"
-          href="/favicon-96x96.png"
-          sizes="96x96"
-        />
-        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
-        <meta name="apple-mobile-web-app-title" content="YK Innovations" />
-        <link rel="manifest" href="/site.webmanifest" />
-      </Head>
       <body className={cn(inter.className, "h-full")}>
         <Navbar>
           <NavLinks />
